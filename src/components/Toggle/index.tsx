51b19087f9de573684801473e7f1fed2baa4c745
Caio18-cosenza <caiiocosenza2020@gmail.com>
2023-02-28 21:36:45 -0300

    first commit

---
+++ src/components/Toggle/index.tsx
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+import { Pressable } from 'react-native';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  Easing,
+  interpolateColor,
+} from 'react-native-reanimated';
+
+import ImageSol from '../../images/sol.png';
+import ImageLua from '../../images/lua.png';
+
+import { styles } from './style';
+
+const ButtonAnimated = Animated.createAnimatedComponent(Pressable);
+
+export const Toggle = () => {
+  const [image, setImage] = useState(ImageSol);
+  const toggleAnimated = useSharedValue(0);
+
+  const styleButtonAnimation = useAnimatedStyle(() => {
+    return {
+      borderColor: interpolateColor(
+        toggleAnimated.value,
+        [0, 1],
+        ['blue', '#111']
+      ),
+    };
+  });
+
+  const styleImageAnimation = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: withTiming(toggleAnimated.value === 0 ? 0 : 90, {
+            duration: 1100,
+            easing: Easing.elastic(2),
+          }),
+        },
+      ],
+    };
+  });
+
+  const styleBackgroundAnimation = useAnimatedStyle(() => {
+    return {
+      backgroundColor: interpolateColor(
+        toggleAnimated.value,
+        [0, 1],
+        ['#42bff5', '#282929']
+      ),
+    };
+  });
+
+  return (
+    <ButtonAnimated
+      onPress={() => {
+        if (toggleAnimated.value === 1) {
+          toggleAnimated.value = 0;
+          setImage(ImageSol);
+        } else {
+          toggleAnimated.value = 1;
+          setImage(ImageLua);
+        }
+      }}
+      style={[styles.container, styleButtonAnimation]}
+    >
+      <Animated.View style={[styles.background, styleBackgroundAnimation]}>
+        <Animated.Image
+          source={image}
+          style={[styles.image, styleImageAnimation]}
+        />
+      </Animated.View>
+    </ButtonAnimated>
+  );
+};
---
+++ src/components/Toggle/style.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    borderWidth: 4,
+    width: 150,
+    height: 50,
+    borderRadius: 50,
+    overflow: 'hidden',
+  },
+  background: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    paddingLeft: 5,
+  },
+  image: {
+    width: 40,
+    height: 40,
+  },
+});
