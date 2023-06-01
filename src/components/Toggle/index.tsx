86f5122d555eecc8dcca88dda6af43a7ce7387f3
Caio18-cosenza <caiiocosenza2020@gmail.com>
2023-05-31 22:21:50 -0300

    Design Only

--- src/components/Toggle/index.tsx
+++ src/components/Toggle/index.tsx
@@ -19,62 +19,12 @@ const ButtonAnimated = Animated.createAnimatedComponent(Pressable);
 
 export const Toggle = () => {
   const [image, setImage] = useState(ImageSol);
-  const toggleAnimated = useSharedValue(0);
   const { changeType } = useMode();
 
-  const styleButtonAnimation = useAnimatedStyle(() => {
-    return {
-      borderColor: interpolateColor(
-        toggleAnimated.value,
-        [0, 1],
-        ['blue', '#111']
-      ),
-    };
-  });
-
-  const styleImageAnimation = useAnimatedStyle(() => {
-    return {
-      transform: [
-        {
-          translateX: withTiming(toggleAnimated.value === 0 ? 0 : 90, {
-            duration: 1100,
-            easing: Easing.elastic(2),
-          }),
-        },
-      ],
-    };
-  });
-
-  const styleBackgroundAnimation = useAnimatedStyle(() => {
-    return {
-      backgroundColor: interpolateColor(
-        toggleAnimated.value,
-        [0, 1],
-        ['#42bff5', '#282929']
-      ),
-    };
-  });
-
   return (
-    <ButtonAnimated
-      onPress={() => {
-        if (toggleAnimated.value === 1) {
-          toggleAnimated.value = 0;
-          setImage(ImageSol);
-          changeType('sun');
-        } else {
-          toggleAnimated.value = 1;
-          setImage(ImageLua);
-          changeType('moon');
-        }
-      }}
-      style={[styles.container, styleButtonAnimation]}
-    >
-      <Animated.View style={[styles.background, styleBackgroundAnimation]}>
-        <Animated.Image
-          source={image}
-          style={[styles.image, styleImageAnimation]}
-        />
+    <ButtonAnimated onPress={() => {}} style={[styles.container]}>
+      <Animated.View style={[styles.background]}>
+        <Animated.Image source={image} style={[styles.image]} />
       </Animated.View>
     </ButtonAnimated>
   );
